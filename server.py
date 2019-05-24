fa10f82ceb56cebf80f5bfd1c198e19fa60914b8
OleksandrVakhno <vakhno.olexandrlutsk@gmail.com>
2019-05-24 17:56:31 -0400

    Added user average height calculator using PostreSQL

--- server.py
+++ server.py
@@ -4,10 +4,26 @@ import datetime
 from bokeh.plotting import figure
 from bokeh.embed import components
 from bokeh.resources import CDN
-from flask import request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+from support_func.user_height import send_email
 
 
 app =Flask(__name__)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgre1234@localhost/height_collector'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://yezqpyvxnvdntv:9386ee0cf33369ac70b28c873855a56f473d297f539f15e4b520d62f82950d22@ec2-54-83-61-142.compute-1.amazonaws.com:5432/d1roobj6kravp?sslmode=require'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =False
+db = SQLAlchemy(app)
+
+class Data(db.Model):
+    __tablename__ = "data"
+    id = db.Column(db.Integer, primary_key = True)
+    _email = db.Column(db.String(120), unique=True)
+    _height = db.Column(db.Integer)
+
+    def __init__(self, email, height):
+        self._email =email
+        self._height= height
 
 @app.route('/')
 def home():
@@ -55,6 +71,28 @@ def finance_graph():
     cdn_js = CDN.js_files[0]
     cdn_css = CDN.css_files[0]
     return render_template("finance_graph.html", script1= script1, div1=div1, cdn_css=cdn_css, cdn_js=cdn_js, request_str=request_str)
+
+@app.route('/user_height', methods=['GET','POST'])
+def user_height():
+    if request.method == "POST":
+        email = request.form["email"]
+        height = request.form["height"]
+        if not db.session.query(Data).filter(Data._email==email).count():
+            data = Data(email,height)
+            db.session.add(data)
+            db.session.commit()
+            warning=""
+        else:
+            db.session.query(Data).filter(Data._email==email).update({Data._height: height})
+            db.session.commit()
+            warning ="Entry with the same email was found. The value was updated."
+
+        average = round(db.session.query(func.avg(Data._height)).scalar(),1)
+        count = db.session.query(Data._height).count()
+        #send_email(email,height, average, count)
+        return render_template('user_height.html', average=average, count = count, warning = warning)
+    else:
+        return render_template('user_height.html')
 
 if __name__=="__main__":
     app.run(debug=True)
---
+++ support_func/user_height.py
@@ -0,0 +1,21 @@
+from email.mime.text import MIMEText
+from smtplib import SMTP
+
+def send_email(email, height, average, count):
+    from_email = "vakhno.alex@gmail.com"
+    from_password = "12345678Va!"
+    to_email=email
+
+    subject="Height data"
+    message ="Your height is <strong>%s</strong>. <br> Average height of <strong>%s</strong> users is <strong>%s</strong>.<br>" %(height, count, average)
+
+    mail = MIMEText(message, 'html')
+    mail['Subject']= subject
+    mail['To']= to_email
+    mail['From']=from_email
+
+    gmail = SMTP('smtp.gmail.com', 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(from_email,from_password)
+    gmail.send_message(mail)
